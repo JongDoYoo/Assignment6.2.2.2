291aa64dd163d0e84dc8e7acf835b4d306859338
JongD <JongD@JONGDO>
2024-06-12 09:00:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment6._2._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4 };
+            int[] newArray = { 1, 2, 3, 4 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 1;
+
+                foreach (int currentNum in array)
+                {
+                    if (array[i] != currentNum)
+                    {
+                        count *= currentNum;
+                    }
+                }
+                newArray[i] = count;
+
+            }
+            Console.Write("[");
+            foreach (int i in newArray)
+            {
+                Console.Write(i+",");
+            }
+            Console.Write("]");
+        }
+    }
+}
